e1d522d67c3f31a52a22c30c06ec773b26021625
Jason Forte <fortejas@amazon.com>
2021-11-10 10:58:50 +0000

    My Landing Page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import WaitingListForm from './components/WaitingListForm'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my Landing Page</h1>
+      <WaitingListForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/WaitingListForm.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react"
+import { API } from 'aws-amplify'
+
+const apiName = 'contacts'
+const apiPath = '/contacts'
+
+const WaitingListForm = () => {
+
+    const [formState, updateFormState] = useState({isLoading: false, error: false, msg: ''})
+
+    const [email, setEmail] = useState('')
+
+
+    if (formState.isLoading) {
+        return (
+            <div>Loading</div>
+        )
+    }
+
+    const submitForm = (evt) => {
+        evt.preventDefault()
+
+        updateFormState((state) => ({...state, isLoading: true}))
+
+        // Submit to API
+        API.post(apiName, apiPath, { body: { email: email, created: new Date().toISOString() } })
+            .then(() => {
+                updateFormState((state) => ({...state, isLoading: false, msg: 'Success'}))
+            })
+            .catch(err => {
+                if (err) {
+                    updateFormState((state) => ({...state, isLoading: false, error: err}))
+                }
+            })
+    }
+
+    return (
+        <form onSubmit={submitForm}>
+            <h2>Are you interested in trying it out?</h2>
+            <input
+                type="email"
+                placeholder="Email Address"
+                name="email"
+                value={email}
+                onChange={(evt) => setEmail(evt.target.value)}
+            />
+            <input type="submit" value="Sign me up!!"/>
+
+            <br/>
+            { (formState.msg !== '') ? formState.msg : '' }
+            { (formState.error !== '') ? formState.error : '' }
+        </form>
+    )
+}
+
+
+export default WaitingListForm
